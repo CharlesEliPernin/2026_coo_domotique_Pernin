04a6cc8c27bda3ffd3c76771f533f35a897943d5
CharlesEliPernin <chales-eli.pernin2@etu.univ-lorraine.fr>
2026-04-03 09:27:48 +0200

    Commit de base avec le bon projet

---
+++ src/Telecommande.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class Telecommande{
+
+    private List<Lampe> lampes;
+
+    public Telecommande(){
+        throw new Error (" code non ecrit ");
+    }
+
+    public void ajouterLampe(Lampe l){
+        throw new Error (" code non ecrit ");
+    }
+
+    public void activerLampe(int indice){
+        throw new Error (" code non ecrit ");
+    }
+
+    public void desactiverLampe(int indice){
+
+    }
+
+    public void activerTout(){
+        throw new Error (" code non ecrit ");
+    }
+
+    public String toString(){
+        throw new Error (" code non ecrit ");
+    }
+}
