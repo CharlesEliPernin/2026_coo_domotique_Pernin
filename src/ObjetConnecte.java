56c4036b90d878c6f7e4bd58d883aff188e1e7a2
CharlesEliPernin <chales-eli.pernin2@etu.univ-lorraine.fr>
2026-04-07 09:28:24 +0200

    Implementation Interface

---
+++ src/ObjetConnecte.java
@@ -0,0 +1,5 @@
+public interface ObjetConnecte {
+    public void allumer();
+    public void eteindre();
+    public String toString();
+}
--- src/Telecommande.java
+++ src/Telecommande.java
@@ -3,72 +3,38 @@ import java.util.List;
 
 public class Telecommande{
 
-    private List<Lampe> lampes;
-    private List<Hifi> hifis;
+    private List<ObjetConnecte> objetsConnectes;
 
-    public Telecommande() {
-        this.lampes = new ArrayList<Lampe>(0);
-
-    }
-
-    public void ajouterLampe(Lampe l){
-        this.lampes.add(l);
-    }
-    public void ajouterHifi(Hifi h){
-        this.hifis.add(h);
-    }
-
-    public void activerLampe(int indice) {
-        if (indice < lampes.size()) {
-            this.lampes.get(indice).allumer();
+    public void allumer(int indice){
+        if (indice < objetsConnectes.size()){
+            objetsConnectes.get(indice).allumer();
         }
     }
-    public void activerHifi(int indice) {
-        if (indice < hifis.size()) {
-            this.hifis.get(indice).allumer();
+    public void eteindre(int indice){
+        if (indice < objetsConnectes.size()){
+            objetsConnectes.get(indice).eteindre();
         }
     }
 
-    public void desactiverLampe(int indice) {
-        if (indice < lampes.size()) {
-            this.lampes.get(indice).eteindre();
-        }
-    }
-    public void desactiverHifi(int indice) {
-        if (indice < hifis.size()) {
-            this.hifis.get(indice).eteindre();
+    public void allumerTout(){
+        for (ObjetConnecte obj : objetsConnectes){
+            obj.allumer();
         }
     }
 
-    public void activerTout(){
-        for (Lampe elt : this.lampes){
-            elt.allumer();
-        }
+    public void ajouterObjet(ObjetConnecte obj){
+        objetsConnectes.add(obj);
     }
 
-    public void activerToutHifi(){
-        for (Hifi elt : this.hifis){
-            elt.allumer();
-        }
-    }
-
-    public List<Lampe> getLampes(){
-        return this.lampes;
-    }
-
-    public List<Hifi> getHifis(){
-        return this.hifis;
+    public List<ObjetConnecte> getObjetsConnectes() {
+        return objetsConnectes;
     }
 
     public String toString(){
         String res = "";
-        for (int i = 0; i < this.lampes.size(); i++){
-            res += "Lampe " + i + " " + this.lampes.get(i).toString() + "\n";
+        for (ObjetConnecte obj : objetsConnectes){
+            res += obj.toString() + "\n";
         }
-        for (int i = 0; i < this.hifis.size(); i++){
-            res += "Hifi " + i + " " + this.hifis.get(i).toString() + "\n";
-        }
-
         return res;
     }
 }
