cb4bb754e5552a128b20c57213c4dea74f7309ca
CharlesEliPernin <chales-eli.pernin2@etu.univ-lorraine.fr>
2026-04-07 08:27:57 +0200

    Hifi ajoutée

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -4,6 +4,7 @@ import java.util.List;
 public class Telecommande{
 
     private List<Lampe> lampes;
+    private List<Hifi> hifis;
 
     public Telecommande() {
         this.lampes = new ArrayList<Lampe>(0);
@@ -13,16 +14,29 @@ public class Telecommande{
     public void ajouterLampe(Lampe l){
         this.lampes.add(l);
     }
+    public void ajouterHifi(Hifi h){
+        this.hifis.add(h);
+    }
 
     public void activerLampe(int indice) {
         if (indice < lampes.size()) {
             this.lampes.get(indice).allumer();
         }
     }
+    public void activerHifi(int indice) {
+        if (indice < hifis.size()) {
+            this.hifis.get(indice).allumer();
+        }
+    }
 
     public void desactiverLampe(int indice) {
         if (indice < lampes.size()) {
             this.lampes.get(indice).eteindre();
+        }
+    }
+    public void desactiverHifi(int indice) {
+        if (indice < hifis.size()) {
+            this.hifis.get(indice).eteindre();
         }
     }
 
@@ -32,14 +46,27 @@ public class Telecommande{
         }
     }
 
+    public void activerToutHifi(){
+        for (Hifi elt : this.hifis){
+            elt.allumer();
+        }
+    }
+
     public List<Lampe> getLampes(){
         return this.lampes;
+    }
+
+    public List<Hifi> getHifis(){
+        return this.hifis;
     }
 
     public String toString(){
         String res = "";
         for (int i = 0; i < this.lampes.size(); i++){
             res += "Lampe " + i + " " + this.lampes.get(i).toString() + "\n";
+        }
+        for (int i = 0; i < this.hifis.size(); i++){
+            res += "Hifi " + i +"\n";
         }
 
         return res;
